8062b302e83773dbd874d76566a95b068224a97a
danzori <main.danzori@gmail.com>
2026-07-21 20:38:13 +0300

    feat(domain): add refresh token entity and errors

--- internal/domain/errors.go
+++ internal/domain/errors.go
@@ -3,6 +3,8 @@ package domain
 import "errors"
 
 var (
-	ErrEmailTaken   = errors.New("email already taken")
-	ErrUserNotFound = errors.New("user not found")
+	ErrEmailTaken           = errors.New("email already taken")
+	ErrUserNotFound         = errors.New("user not found")
+	ErrRefreshTokenNotFound = errors.New("refresh token not found")
+	ErrRefreshTokenReused   = errors.New("refresh token reused")
 )
