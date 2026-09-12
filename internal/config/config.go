9eceb2eeb857226ee70e6f6f9244ae0ff3667b66
danzori <main.danzori@gmail.com>
2026-09-12 13:06:55 +0300

    feat(config): add postgres pool settings

--- internal/config/config.go
+++ internal/config/config.go
@@ -19,7 +19,14 @@ type GRPC struct {
 }
 
 type Postgres struct {
-	DSN string `yaml:"dsn"`
+	DSN               string        `validate:"required" yaml:"dsn"`
+	MaxConns          int32         `yaml:"max_conns"`
+	MinConns          int32         `yaml:"min_conns"`
+	MaxConnLifetime   time.Duration `yaml:"max_conn_lifetime"`
+	MaxConnIdleTime   time.Duration `yaml:"max_conn_idle_time"`
+	HealthCheckPeriod time.Duration `yaml:"health_check_period"`
+	ConnectTimeout    time.Duration `yaml:"connect_timeout"`
+	PingTimeout       time.Duration `yaml:"ping_timeout"`
 }
 
 type Log struct {
