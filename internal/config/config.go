65c684733277617c00620181faab39b2d62e46b5
danzori <main.danzori@gmail.com>
2026-09-17 18:51:07 +0300

    feat(config): add auth and password settings

--- internal/config/config.go
+++ internal/config/config.go
@@ -6,10 +6,12 @@ import (
 )
 
 type Config struct {
+	ServiceName     string        `validate:"required" yaml:"service_name"`
 	ShutdownTimeout time.Duration `validate:"required" yaml:"shutdown_timeout"`
 
 	GRPC     GRPC     `yaml:"grpc"`
 	Postgres Postgres `yaml:"postgres"`
+	Auth     Auth     `yaml:"auth"`
 	Health   Health   `yaml:"health"`
 	Log      Log      `yaml:"log"`
 }
@@ -28,6 +30,42 @@ type Postgres struct {
 	HealthCheckPeriod time.Duration `yaml:"health_check_period"`
 	ConnectTimeout    time.Duration `yaml:"connect_timeout"`
 	PingTimeout       time.Duration `yaml:"ping_timeout"`
+}
+
+type Auth struct {
+	Access   Access   `yaml:"access"`
+	Refresh  Refresh  `yaml:"refresh"`
+	Session  Session  `yaml:"session"`
+	Password Password `yaml:"password"`
+}
+
+type Access struct {
+	TTL            time.Duration `validate:"required" yaml:"ttl"`
+	Issuer         string        `validate:"required" yaml:"issuer"`
+	Audience       []string      `validate:"required,min=1,dive,required" yaml:"audience"`
+	PrivateKeyPath string        `validate:"required" yaml:"private_key_path"`
+	PublicKeyPath  string        `validate:"required" yaml:"public_key_path"`
+}
+
+type Refresh struct {
+	Length int `validate:"required" yaml:"length"`
+}
+
+type Session struct {
+	TTL time.Duration `validate:"required" yaml:"ttl"`
+}
+
+type Password struct {
+	Argon2ID Argon2ID `yaml:"argon2id"`
+}
+
+type Argon2ID struct {
+	MemoryKiB   uint32 `validate:"required" yaml:"memory_kib"`
+	Iterations  uint32 `validate:"required" yaml:"iterations"`
+	Parallelism uint8  `validate:"required" yaml:"parallelism"`
+	SaltLength  uint32 `validate:"required" yaml:"salt_length"`
+	KeyLength   uint32 `validate:"required" yaml:"key_length"`
+	MaxInFlight int    `validate:"required" yaml:"max_in_flight"`
 }
 
 type Health struct {
