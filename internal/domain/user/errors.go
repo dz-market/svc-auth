6fee4fccb0f20712c292126ea225719ec9a59f7e
danzori <main.danzori@gmail.com>
2026-09-18 23:24:00 +0300

    feat(auth): add login use case

--- internal/domain/user/errors.go
+++ internal/domain/user/errors.go
@@ -3,6 +3,7 @@ package user
 import "errors"
 
 var (
-	ErrEmailTaken = errors.New("email is already taken")
-	ErrNotFound   = errors.New("user not found")
+	ErrEmailTaken         = errors.New("email is already taken")
+	ErrNotFound           = errors.New("user not found")
+	ErrInvalidCredentials = errors.New("invalid credentials")
 )
